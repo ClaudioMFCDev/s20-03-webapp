b96907c03466de53bca3303d86cb4b16d4d5b889
M-Azariel <walter.azariel.moreno@gmail.com>
2025-01-24 02:40:54 -0300

    utilizando componente combobox

--- frontend/src/app/(pages)/(auth)/teacher/page.tsx
+++ frontend/src/app/(pages)/(auth)/teacher/page.tsx
@@ -1,13 +1,19 @@
 "use client"
 
 import { CardTeacher } from "#/src/app/components/molecules/card-teacher"
+import { ComboboxDemo } from "#/src/app/components/molecules/combobox";
 import { Users, Calculator } from 'lucide-react';
 
 
 export default function TeacherPage(){
     return (
         <div>
-
+            <aside className="flex justify-between items-center mt-12 mb-14">
+                <h1 className="text-4xl">Bienvenido/a, Amanda martinez</h1>
+                <div className="mr-56">
+                    <ComboboxDemo/>
+                </div>
+            </aside>
             <section className="flex gap-7">
                 <CardTeacher title={"Total de estudiantes en la comision"} info={13} icon={<Users className=" w-4 h-4"/>}/>
                 <CardTeacher title={"Promedio de la comision"} info={8.6} icon={<Calculator className=" w-4 h-4"/>}/>
