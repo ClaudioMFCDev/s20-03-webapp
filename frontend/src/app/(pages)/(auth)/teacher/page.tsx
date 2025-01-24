05e8e1507bab504427a3b6bdda1abbccff8661f6
M-Azariel <walter.azariel.moreno@gmail.com>
2025-01-24 01:58:01 -0300

    utilizando los componentes card de teacher en la pagina de teacher

--- frontend/src/app/(pages)/(auth)/teacher/page.tsx
+++ frontend/src/app/(pages)/(auth)/teacher/page.tsx
@@ -1,12 +1,18 @@
 "use client"
 
-
+import { CardTeacher } from "#/src/app/components/molecules/card-teacher"
+import { Users, Calculator } from 'lucide-react';
 
 
 export default function TeacherPage(){
     return (
         <div>
 
+            <section className="flex gap-7">
+                <CardTeacher title={"Total de estudiantes en la comision"} info={13} icon={<Users className=" w-4 h-4"/>}/>
+                <CardTeacher title={"Promedio de la comision"} info={8.6} icon={<Calculator className=" w-4 h-4"/>}/>
+                <CardTeacher title={"Tareas hechas"} info={<span>{10} / {13}</span> } icon={<Users className=" w-4 h-4"/>}/>
+            </section>
         </div>
         
     )
