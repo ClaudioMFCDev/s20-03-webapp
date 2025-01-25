672f660586514d30b3ff97efa0853e61555f43cc
Eduardo <ejrb1234@gmail.com>
2025-01-25 16:56:30 -0400

    feat(app): Formularios de autenticación
    Se refactorizó el fondo de las pantallas de autenticación, además de utilizaron los schemas de Zod con RHF en los formularios

--- frontend/src/app/(pages)/(auth)/sign-in/page.tsx
+++ frontend/src/app/(pages)/(auth)/sign-in/page.tsx
@@ -1,108 +1,31 @@
-'use client'
+import { FormSignIn } from '@/components/organisms/forms/form-sign-in'
+import { Background } from '@/components/molecules/background'
+import { Metadata } from 'next'
+import Link from 'next/link'
 
-import { useForm } from 'react-hook-form'
-import { motion } from 'framer-motion'
-import { Input } from '@/components/ui/input'
-import { Button } from '@/components/ui/button'
-import { useEffect } from 'react'
+export const metadata: Metadata = {
+  title: 'Iniciar sesión',
+  // description: ""
+}
 
 export default function SignInPage() {
-  const ICON_COUNT = 10
-
-  const generateRandomPosition = () => ({
-    x: Math.random() * window.innerWidth - window.innerWidth / 4,
-    y: Math.random() * window.innerHeight - window.innerHeight / 4,
-  })
-
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm()
-
-  const onSubmit = handleSubmit(data => {
-    console.log(data)
-  })
-  useEffect(() => {
-    console.log('App component mounted or updated')
-  })
-
   return (
-    <section className="relative z-10 flex h-screen w-full items-center justify-start overflow-hidden bg-[#4169E1]">
-      {Array.from({ length: ICON_COUNT }).map((_, index) => {
-        const initialPosition = generateRandomPosition()
-
-        return (
-          <motion.div
-            key={index}
-            initial={initialPosition}
-            animate={{
-              x: [initialPosition.x, -initialPosition.x],
-              y: [initialPosition.y, -initialPosition.y],
-            }}
-            transition={{
-              duration: 15,
-              repeat: Infinity,
-              repeatType: 'loop',
-              ease: 'linear',
-            }}
-            className="absolute"
-          >
-            <div className="h-20 w-20 rounded-full bg-gradient-to-b via-gray-200 to-gray-400 shadow-2xl shadow-black" />
-          </motion.div>
-        )
-      })}
-
-      <section className="flex h-full w-full justify-end">
-        <form
-          onSubmit={onSubmit}
-          className="z-50 flex h-full flex-col items-center justify-center border border-white bg-[#fcfcfcb4] shadow-2xl shadow-black backdrop-blur-md lg:w-[50%]"
-        >
-          <h1 className="text-2xl font-bold">Sing In</h1>
-
-          <div className="mb-3 mt-5">
-            <label htmlFor="email">Correo</label>
-            <Input
-              type="email"
-              className="relative mt-2 w-96"
-              {...register('email', {
-                required: { value: true, message: 'correo es requerido' },
-                pattern: {
-                  value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
-                  message: 'Correo inválido',
-                },
-              })}
-            />
-            {errors.email && (
-              <span className="mt-2 text-xs text-red-800">
-                {errors.email.message as string}
-              </span>
-            )}
-          </div>
-
-          <div className="mb-3 mt-5">
-            <label htmlFor="password">Password</label>
-            <Input
-              type="password"
-              className="mt-2 w-96"
-              {...register('password', {
-                required: { value: true, message: 'Password es requerido' },
-              })}
-            />
-            {errors.password && (
-              <span className="text-xs text-red-800">
-                {errors.password.message as string}
-              </span>
-            )}
-          </div>
-          <Button
-            type="submit"
-            className="mt-10 w-96 rounded-sm bg-slate-950 py-[3px] text-white"
-          >
-            Login
-          </Button>
-        </form>
-      </section>
-    </section>
+    <Background justifyEnd>
+      <div className="flex h-full w-full flex-col items-center justify-center gap-8 bg-white p-8 backdrop-blur-md lg:w-1/2">
+        <div className="flex flex-col gap-2 text-center">
+          <h1 className="text-3xl font-bold">Iniciar sesión</h1>
+          <h2 className="text-md text-muted-foreground">
+            Ingresa tu correo electrónico y contraseña para iniciar sesión
+          </h2>
+        </div>
+        <FormSignIn />
+        <div className="flex gap-2 text-center text-sm">
+          <span className="text-muted-foreground">¿No tienes una cuenta?</span>
+          <Link href="/sign-up" className="hover:underline">
+            ¡Regístrate!
+          </Link>
+        </div>
+      </div>
+    </Background>
   )
 }
--- frontend/src/validations/schemas.ts
+++ frontend/src/validations/schemas.ts
@@ -27,3 +27,27 @@ export const ProfileFormSchema = z
     message: 'Debes aceptar los términos y condiciones',
     path: ['security_emails'],
   })
+
+export const SignInFormSchema = z.object({
+  email: z.string().email({
+    message: 'El correo electrónico debe ser válido',
+  }),
+  password: z.string().min(8, {
+    message: 'La contraseña debe tener al menos 8 caracteres',
+  }),
+})
+
+export const SignUpFormSchema = z.object({
+  name: z.string().min(3, {
+    message: 'El nombre debe tener al menos 3 caracteres',
+  }),
+  lastName: z.string().min(3, {
+    message: 'El apellido debe tener al menos 3 caracteres',
+  }),
+  email: z.string().email({
+    message: 'El correo electrónico debe ser válido',
+  }),
+  password: z.string().min(8, {
+    message: 'La contraseña debe tener al menos 8 caracteres',
+  }),
+})
