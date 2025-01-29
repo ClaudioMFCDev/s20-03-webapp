c86aa304c30ed56481c5e950ef6fae91fe033863
M-Azariel <walter.azariel.moreno@gmail.com>
2025-01-29 00:44:50 -0300

    agregando componente payments

---
+++ frontend/src/data/payments.data.tsx
@@ -0,0 +1,32 @@
+import { v4 as uuidV4 } from "uuid";
+import { uniqueNamesGenerator, Config, names } from "unique-names-generator";
+
+const config: Config = {
+    dictionaries: [names],
+};
+
+
+export type Payment = {
+    id: string;
+    amount: number;
+    status: "success" | "failed";
+    email: string;
+    alumnName: string;
+};
+
+const randomStatus = () => {
+    const statuses = [ "success"] as const;
+    return statuses[Math.floor(Math.random() * statuses.length)];
+};
+
+
+
+export const payments: Payment[] = Array.from({ length: 100 }, (_) => {
+    const randomName = uniqueNamesGenerator(config);
+
+    return {
+        id: uuidV4(),
+        status: randomStatus(),
+        alumnName: randomName,
+    };
+});
