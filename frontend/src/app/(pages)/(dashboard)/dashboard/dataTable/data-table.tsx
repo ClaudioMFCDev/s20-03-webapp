069761f1405849a4b02752aadbb5444dd5b9246d
M-Azariel <walter.azariel.moreno@gmail.com>
2025-02-01 02:45:46 -0300

    terminado el deshboard de la comision

--- frontend/src/app/(pages)/(dashboard)/dashboard/dataTable/data-table.tsx
+++ frontend/src/app/(pages)/(dashboard)/dashboard/dataTable/data-table.tsx
@@ -33,75 +33,82 @@ export function DataTable<TData, TValue>({
     columns,
     getCoreRowModel: getCoreRowModel(),
     getPaginationRowModel: getPaginationRowModel(),
+    initialState: {
+        pagination: {
+            pageSize: 11, 
+        },
+    },
   })
 
-  return (
-    <div className="flex flex-col items-center justify-between rounded-md border p-5">
-      <Table className="flex flex-col justify-between">
-        <TableHeader className="flex justify-between">
-          {table.getHeaderGroups().map(headerGroup => (
-            <TableRow
-              className="flex justify-between gap-28 lg:gap-96 2xl:gap-[30rem]"
-              key={headerGroup.id}
-            >
-              {headerGroup.headers.map(header => {
-                return (
-                  <TableHead className="" key={header.id}>
-                    {header.isPlaceholder
-                      ? null
-                      : flexRender(
-                          header.column.columnDef.header,
-                          header.getContext()
-                        )}
-                  </TableHead>
-                )
-              })}
-            </TableRow>
-          ))}
-        </TableHeader>
-        <TableBody>
-          {table.getRowModel().rows?.length ? (
-            table.getRowModel().rows.map(row => (
-              <TableRow
-                className="flex justify-between"
-                key={row.id}
-                data-state={row.getIsSelected() && 'selected'}
-              >
-                {row.getVisibleCells().map(cell => (
-                  <TableCell className="mr-12" key={cell.id}>
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                  </TableCell>
-                ))}
-              </TableRow>
-            ))
-          ) : (
-            <TableRow>
-              <TableCell colSpan={columns.length} className="h-24 text-center">
-                No results.
-              </TableCell>
-            </TableRow>
-          )}
-        </TableBody>
-      </Table>
-
-      <div className="flex gap-5 px-5">
-        <Button
-          variant="outline"
-          size="sm"
-          onClick={() => table.previousPage()}
-          disabled={!table.getCanPreviousPage()}
-        >
-          Previous
-        </Button>
-        <Button
-          variant="outline"
-          size="sm"
-          onClick={() => table.nextPage()}
-          disabled={!table.getCanNextPage()}
-        >
-          Next
-        </Button>
-      </div>
-    </div>
-  )
+    return (
+        <div className="flex flex-col h-[760px] justify-between rounded-md border p-5">
+            {/* Tabla */}
+            <Table className="flex-1">
+                <TableHeader className=''>
+                    {table.getHeaderGroups().map((headerGroup) => (
+                        <TableRow key={headerGroup.id}>
+                            {headerGroup.headers.map((header) => (
+                                <TableHead key={header.id}>
+                                    {header.isPlaceholder
+                                        ? null
+                                        : flexRender(
+                                            header.column.columnDef.header,
+                                            header.getContext()
+                                        )}
+                                </TableHead>
+                            ))}
+                        </TableRow>
+                    ))}
+                </TableHeader>
+                <TableBody>
+                    {table.getRowModel().rows?.length ? (
+                        table.getRowModel().rows.map((row) => (
+                            <TableRow
+                                key={row.id}
+                                data-state={row.getIsSelected() && "selected"}
+                                
+                            >
+                                {row.getVisibleCells().map((cell) => (
+                                    <TableCell className='' key={cell.id}>
+                                        {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                    </TableCell>
+                                ))}
+                            </TableRow>
+                        ))
+                    ) : (
+                        <TableRow>
+                            <TableCell colSpan={columns.length} className="h-24 text-center">
+                                No results.
+                            </TableCell>
+                        </TableRow>
+                    )}
+                </TableBody>
+            </Table>
+                  
+            <div className="flex justify-between items-center mt-5">
+                <div className="flex gap-5">
+                    <Button
+                        variant="outline"
+                        size="sm"
+                        onClick={() => table.previousPage()}
+                        disabled={!table.getCanPreviousPage()}
+                    >
+                        Previous
+                    </Button>
+                    <Button
+                        variant="outline"
+                        size="sm"
+                        onClick={() => table.nextPage()}
+                        disabled={!table.getCanNextPage()}
+                    >
+                        Next
+                    </Button>
+                </div>
+                  
+                <Button type="submit" className="w-[78px]">
+                    Update
+                </Button>
+            </div>
+        </div>
+    )
 }
